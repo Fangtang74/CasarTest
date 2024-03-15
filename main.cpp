70b254b544eff93f33e45a64f5ddef0c8717a082
Armin <izhaorm@qq.com>
2024-03-15 11:35:32 +0000

    111

--- main.cpp
+++ main.cpp
@@ -20,11 +20,11 @@ int main()
         file1 << i << "," << fengSchemeUav(i) << "," << karSchemeUav(i) << "," << puSchemeUav(i) << "," << ourSchemeUav(i) << "\n";
     }
     file1.close();
-    std::ofstream file2("gsTime.csv");
+    std::ofstream file2("totalTime.csv");
     file2 << "Nums,feng,kar,pu,our\n";
     for (int i = 1; i <= 50; i += 1)
     {
-        file2 << i << "," << fengSchemeGs(i) << "," << karSchemeGs(i) << "," << puSchemeGs(i) << "," << ourSchemeGs(i) << "\n";
+        file2 << i << "," << fengSchemeGs(i) + fengSchemeUav(i) << "," << karSchemeGs(i) + karSchemeUav(i) << "," << puSchemeGs(i) + puSchemeUav(i) << "," << ourSchemeGs(i) + ourSchemeUav(i) << "\n";
     }
     file2.close();
     // getFuncTime();
